beb8bf570fff5c265f471cd17454bdf0f05a36ee
gioisco <just.gioisco@gmail.com>
2023-01-23 04:22:37 +0100

    Regenerate save csv and generate output with pandas

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import os
 import pyfiglet
 import scrapetube
 import textwrap
+from datetime import datetime
+import pandas as pd
 
 from utils import extract_number
 
 ascii_art = pyfiglet.figlet_format("Youtube scraper")
 print(ascii_art)
+
 
 channel_url = input("Insert channel URL: ")
 if channel_url == "":
@@ -29,28 +33,30 @@ print("Searching {} videos for {}".format(limit, channel_url))
 
 videos = scrapetube.get_channel(channel_url=channel_url, limit=limit)
 
+now = datetime.now()
 videos_data = []
 x = -1
 for video in videos:
+    video_id = video['videoId']
     title = video['title']['runs'][x+1]['text']
     view_count = extract_number(video['viewCountText']['simpleText'])
-    videos_data.append((view_count, title))
+    videos_data.append((now, video_id, view_count, title))
 
-# Configure output
-views_width = len(str(max(v[0] for v in videos_data))) + 2
-title_width = 60
-format_string = "{{:>{}s}}  {{:<{}s}}".format(views_width, title_width)
+df = pd.DataFrame(videos_data, columns=['date_time', 'video_id', 'view_count', 'title'])
+print(df)
 
-# Print list videos
-print(format_string.format("Views", "Title"))
-for view_count, title in videos_data:
-    print(format_string.format(str(view_count), textwrap.shorten(str(title), width=title_width, placeholder="...")))
-print("")
-
-# Print average views
-average_views = round(sum(v[0] for v in videos_data) / len(videos_data))
-print("The average views is " + str(average_views))
-print("")
+# Save file
+if os.path.isfile('videos_data.csv'):
+    existing_df = pd.read_csv('videos_data.csv', nrows=1)
+    existing_columns = set(existing_df.columns)
+    new_columns = set(df.columns)
+    if existing_columns == new_columns:
+        df.to_csv('videos_data.csv', mode='a', header=False, index=False)
+    else:
+        missing_columns = new_columns.difference(existing_columns)
+        raise ValueError(f"Columns {missing_columns} do not match between new DataFrame and existing csv file.")
+else:
+    df.to_csv('videos_data.csv', index=False)
 
 
 
